b671eefac7906a59de789aadf7a0639983b17c73
jjuya21 <jjuya21@gmail.com>
2024-09-28 21:52:54 +0900

    first commit

---
+++ src/test/java/hh_week2/HhWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hh_week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhWeek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
